ad4c46933e02de6f7424d209ecc964433a33dc3f
André <andre_miguel_costa@hotmail.com>
2024-12-23 11:09:34 +0100

    feat: generate minimal reproducible example

--- src/schema.rs
+++ src/schema.rs
@@ -1,40 +1,21 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    t0 (id) {
+        id -> Int4,
+    }
+}
+
+diesel::table! {
     t1 (id) {
         id -> Int4,
         t0_id -> Int4,
     }
 }
 
-diesel::table! {
-    t2 (id) {
-        id -> Int4,
-        t1_id -> Int4,
-    }
-}
-
-diesel::table! {
-    t3 (id) {
-        id -> Int4,
-        t2_id -> Int4,
-    }
-}
-
-diesel::table! {
-    t4 (id) {
-        id -> Int4,
-        t3_id -> Int4,
-    }
-}
-
-diesel::joinable!(t2 -> t1 (t1_id));
-diesel::joinable!(t3 -> t2 (t2_id));
-diesel::joinable!(t4 -> t3 (t3_id));
+diesel::joinable!(t1 -> t0 (t0_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
+    t0,
     t1,
-    t2,
-    t3,
-    t4,
 );
